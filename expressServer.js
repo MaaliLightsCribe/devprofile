271a07c3b34fe869284598c7ac1b37b4d83ecd7e
Muhammad Farhan <47051361+MaaliLightsCribe@users.noreply.github.com>
2019-09-26 22:18:25 +0500

    Initial commit

---
+++ expressServer.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/', (req, res) => res.send('API Running'))
+
+
+
+
+
+
+
+
+
+// Server Start Setting: 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+
